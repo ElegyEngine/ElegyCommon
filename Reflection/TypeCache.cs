28c44c68c9c3ff9eeb2e8796b6e69ed1c997b473
Admer <49600504+Admer456@users.noreply.github.com>
2023-03-29 18:30:01 +0200

    TypeCache: freeze until mid April

--- Reflection/TypeCache.cs
+++ Reflection/TypeCache.cs
@@ -1,6 +1,8 @@
 ﻿// SPDX-FileCopyrightText: 2022-2023 Admer Šuko
 // SPDX-License-Identifier: MIT
 
+// Stub, will work on it again mid April
+
 using System;
 using System.Reflection;
 
@@ -10,9 +12,9 @@ namespace Elegy.Reflection
 	/// The global type cache.
 	/// It accelerates queries about datatypes.
 	/// </summary>
-	public class TypeCache
+	internal class TypeCache
 	{
-		public IReadOnlyList<TypeInfo> TypeInfos => mTypeInfos;
-		private List<TypeInfo> mTypeInfos = new();
+		//public IReadOnlyList<TypeInfo> TypeInfos => mTypeInfos;
+		//private List<TypeInfo> mTypeInfos = new();
 	}
 }
