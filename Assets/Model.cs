03f681b0a598847ccc6289a7a8880d1b4fd7cbdf
Admer <49600504+Admer456@users.noreply.github.com>
2023-06-23 15:22:20 +0200

    Assets.Model draft

---
+++ Assets/Model.cs
@@ -0,0 +1,18 @@
+﻿// SPDX-FileCopyrightText: 2022-2023 Admer Šuko
+// SPDX-License-Identifier: MIT
+
+using Elegy.Text;
+
+namespace Elegy.Assets
+{
+	/// <summary>
+	/// Raw data for Elegy models. Models can represent NPCs, weapons,
+	/// terrain, 3D UIs, props and other things. Elegy's model format is
+	/// generally concerned with monolithic models for individual entities,
+	/// as opposed to models that host entire scenes and submodels.
+	/// </summary>
+	public class Model
+	{
+
+	}
+}
---
+++ Assets/ModelData.cs
@@ -0,0 +1,37 @@
+﻿// SPDX-FileCopyrightText: 2022-2023 Admer Šuko
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.Assets.ModelData
+{
+	/// <summary>
+	/// All possible vertex data that can be stored in EMFs.
+	/// </summary>
+	public struct Vertex
+	{
+		public Vertex()
+		{
+
+		}
+
+		public Vector3 Position { get; set; } = Vector3.Zero;
+		public Vector3 Normal { get; set; } = Vector3.Up;
+		public Vector3 Tangent { get; set; } = Vector3.Right;
+		public Vector3 Bitangent { get; set; } = Vector3.Forward;
+		public Vector2 Uv1 { get; set; } = Vector2.Zero;
+		public Vector2 Uv2 { get; set; } = Vector2.Zero;
+		public Vector4 Color1 { get; set; } = Vector4.One;
+		public Vector4 Color2 { get; set; } = Vector4.One;
+		public Vector4I BoneIndices { get; set; } = Vector4I.One;
+		public Vector4 BoneWeights { get; set; } = Vector4.One;
+	}
+
+	/// <summary>
+	/// Represents a particular part of a model with a material assigned to it.
+	/// </summary>
+	public class Mesh
+	{
+		public string Name { get; set; } = string.Empty;
+		public List<Vertex> Vertices { get; set; } = new();
+
+	}
+}
