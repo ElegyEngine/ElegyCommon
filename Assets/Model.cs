aac6f2217e42a4dfe9920aabbde97ce6e28b6054
Admer <49600504+Admer456@users.noreply.github.com>
2023-06-27 15:00:00 +0200

    Assets.Model: model data draft

--- Assets/Model.cs
+++ Assets/Model.cs
@@ -1,10 +1,13 @@
 ﻿// SPDX-FileCopyrightText: 2022-2023 Admer Šuko
 // SPDX-License-Identifier: MIT
 
-using Elegy.Text;
+using Elegy.Assets.ModelData;
 
 namespace Elegy.Assets
 {
+	// TODO: Okay, hear me out. What if we have a Mesh asset and a Skeleton asset,
+	// so that we can easily have shared Animation assets?
+
 	/// <summary>
 	/// Raw data for Elegy models. Models can represent NPCs, weapons,
 	/// terrain, 3D UIs, props and other things. Elegy's model format is
@@ -13,6 +16,10 @@ namespace Elegy.Assets
 	/// </summary>
 	public class Model
 	{
+		public string Name { get; set; } = string.Empty;
 
+		public List<ModelData.Mesh> Meshes { get; set; } = new();
+
+		public List<BoneJoint> Joints { get; set; } = new();
 	}
 }
