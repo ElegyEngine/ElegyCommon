4db32807e5a1d33ee036059cc82674c289246f3e
Admer <49600504+Admer456@users.noreply.github.com>
2023-10-23 21:05:30 +0200

    IByteBuffer: documentation

--- Utilities/Interfaces/IByteBuffer.cs
+++ Utilities/Interfaces/IByteBuffer.cs
@@ -29,12 +29,30 @@ namespace Elegy.Utilities.Interfaces
 	/// </summary>
 	public interface IByteBuffer
 	{
+		/// <summary>
+		/// Cursor position.
+		/// </summary>
 		public int Position { get; }
+		/// <summary>
+		/// Buffer data.
+		/// </summary>
 		public IReadOnlyList<byte> Data { get; }
+		/// <summary>
+		/// Span version of <see cref="Data"/>.
+		/// </summary>
 		public ReadOnlySpan<byte> DataSpan { get; }
 
+		/// <summary>
+		/// Advances the cursor <paramref name="bytes"/> number of bytes.
+		/// </summary>
 		public void Advance( int bytes );
+		/// <summary>
+		/// Resets the cursor to the beginning of the buffer.
+		/// </summary>
 		public void ResetPosition();
+		/// <summary>
+		/// Clears the data buffer.
+		/// </summary>
 		public void ResetData();
 
 		/// <summary>
@@ -49,35 +67,119 @@ namespace Elegy.Utilities.Interfaces
 		/// <typeparam name="T">The type of data that gets read.</typeparam>
 		public T Read<T>() where T : unmanaged;
 
+		/// <summary>
+		/// Writes an unsigned byte value and advances 1 byte.
+		/// </summary>
 		public void WriteU8( byte value );
+		/// <summary>
+		/// Writes a boolean value and advances 1 byte.
+		/// </summary>
 		public void WriteBool( bool value );
+		/// <summary>
+		/// Writes an ASCII character value and advances 1 byte.
+		/// </summary>
 		public void WriteChar( char value );
+		/// <summary>
+		/// Writes an enum value and advances 1 byte.
+		/// </summary>
 		public void WriteEnum<T>( T value ) where T : struct, Enum, IConvertible;
+		/// <summary>
+		/// Writes a signed short value and advances 2 bytes.
+		/// </summary>
 		public void WriteI16( short value );
+		/// <summary>
+		/// Writes an unsigned short value and advances 2 bytes.
+		/// </summary>
 		public void WriteU16( ushort value );
+		/// <summary>
+		/// Writes a signed integer value and advances 4 bytes.
+		/// </summary>
 		public void WriteI32( int value );
+		/// <summary>
+		/// Writes an unsigned integer value and advances 4 bytes.
+		/// </summary>
 		public void WriteU32( uint value );
+		/// <summary>
+		/// Writes a signed long value and advances 8 bytes.
+		/// </summary>
 		public void WriteI64( long value );
+		/// <summary>
+		/// Writes an unsigned long value and advances 8 bytes.
+		/// </summary>
 		public void WriteU64( ulong value );
+		/// <summary>
+		/// Writes a half-precision float value and advances 2 bytes.
+		/// </summary>
 		public void WriteF16( Half value );
+		/// <summary>
+		/// Writes a single-precision float value and advances 4 bytes.
+		/// </summary>
 		public void WriteF32( float value );
+		/// <summary>
+		/// Writes a double-precision float value and advances 8 bytes.
+		/// </summary>
 		public void WriteF64( double value );
 
+		/// <summary>
+		/// Reads an unsigned byte value and advances 1 byte.
+		/// </summary>
 		public byte ReadU8();
+		/// <summary>
+		/// Reads a boolean value and advances 1 byte.
+		/// </summary>
 		public bool ReadBool();
+		/// <summary>
+		/// Reads a boolean value and advances 1 byte.
+		/// </summary>
 		public char ReadChar();
+		/// <summary>
+		/// Reads an ASCII character value and advances 1 byte.
+		/// </summary>
 		public T ReadEnum<T>() where T : struct, Enum;
+		/// <summary>
+		/// Reads a signed short value and advances 2 bytes.
+		/// </summary>
 		public short ReadI16();
+		/// <summary>
+		/// Reads an unsigned short value and advances 2 bytes.
+		/// </summary>
 		public ushort ReadU16();
+		/// <summary>
+		/// Reads a signed integer value and advances 4 bytes.
+		/// </summary>
 		public int ReadI32();
+		/// <summary>
+		/// Reads an unsigned integer value and advances 4 bytes.
+		/// </summary>
 		public uint ReadU32();
+		/// <summary>
+		/// Reads a signed long value and advances 8 bytes.
+		/// </summary>
 		public long ReadI64();
+		/// <summary>
+		/// Reads an unsigned long value and advances 8 bytes.
+		/// </summary>
 		public ulong ReadU64();
+		/// <summary>
+		/// Reads a half-precision float value and advances 2 bytes.
+		/// </summary>
 		public Half ReadF16();
+		/// <summary>
+		/// Reads a single-precision float value and advances 4 bytes.
+		/// </summary>
 		public float ReadF32();
+		/// <summary>
+		/// Reads a double-precision float value and advances 8 bytes.
+		/// </summary>
 		public double ReadF64();
 
+		/// <summary>
+		/// Writes/serialises an object that implements <see cref="IByteSerialisable"/>.
+		/// </summary>
 		public void WriteObject<T>( T value ) where T : IByteSerialisable;
+		/// <summary>
+		/// Reads/deserialises an object that implements <see cref="IByteSerialisable"/>.
+		/// </summary>
 		public T ReadObject<T>() where T : IByteSerialisable, new();
 
 		/// <summary>
